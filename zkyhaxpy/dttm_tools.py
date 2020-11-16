ce17d7532361945ec438364c6fbd7fd6970dd8ca
SurasakC <SurasakC@bot.or.th>
2020-11-16 13:50:15 +0700

    Add convert_th_date_to_datetime for dttm_tools.py

--- zkyhaxpy/dttm_tools.py
+++ zkyhaxpy/dttm_tools.py
@@ -1,5 +1,34 @@
 import datetime
+import re
 
+dict_th_month = {
+        'มกราคม':1,
+        'กุมภาพันธ์':2,
+        'มีนาคม':3,
+        'เมษายน':4,
+        'พฤษภาคม':5,
+        'มิถุนายน':6,
+        'กรกฎาคม':7,
+        'สิงหาคม':8,
+        'กันยายน':9,
+        'ตุลาคม':10,
+        'พฤศจิกายน':11,
+        'ธันวาคม':12,
+        'ม.ค.':1,
+        'ก.พ.':2,
+        'มี.ค.':3,
+        'เม.ย.':4,
+        'พ.ค.':5,
+        'มิ.ย.':6,
+        'ก.ค.':7,
+        'ส.ค.':8,
+        'ก.ย.':9,
+        'ต.ค.':10,
+        'พ.ย.':11,
+        'ธ.ค.':12,
+    }
+    
+    
 def dttm_to_yyyymmdd(dttm):
     '''
     Return : a string of current datetime in yyyymmdd_hhmm 
@@ -34,3 +63,42 @@ def yyyymmdd_add_days(yyyymmdd, days):
     '''
     date = datetime.datetime.strptime(yyyymmdd, '%Y%m%d') + datetime.timedelta(days=days)
     return datetime.datetime.strftime(date, '%Y%m%d') 
+
+
+def convert_th_date_to_datetime(str_th_date):
+    '''
+
+    Convert a string of Thai date into datetime.
+
+    Parameters
+    ----------
+    str_th_date: str
+        A string of Thai date (ex. 1 ก.ค. 56, 1 มีนาคม 2563)
+    
+    Returns
+    -------
+    out_datetime : datetime
+    
+	'''
+
+    for str_month, int_month in dict_th_month.items():
+        str_th_date = str_th_date.replace(str_month, str(int_month))
+
+    str_th_date = str_th_date.replace(' ', '-')
+
+    int_day = int(re.findall('^\\d{1,2}-', str_th_date)[0][:-1])
+    
+    int_month = int(re.findall('-\\d{1,2}-', str_th_date)[0][1:-1])  
+        
+    str_year = re.findall('-\\d{2,4}$', str_th_date)[0][1:]
+
+    #year
+    if len(str_year) == 2:
+        int_year = int(str_year) - 543 + 2500
+    elif len(str_year) == 4:
+        int_year = int(str_year) - 543
+    else:
+        int_year = 1900    
+    
+    out_datetime = datetime.datetime(year=int_year, month=int_month, day=int_day)
+    return out_datetime
