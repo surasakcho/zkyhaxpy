41c3c878c28a42e31b288fca76ba57c800caa083
SurasakC <SurasakC@bot.or.th>
2020-12-17 09:30:13 +0700

    Update log_tools.py (fix bug)

--- zkyhaxpy/log_tools.py
+++ zkyhaxpy/log_tools.py
@@ -4,13 +4,15 @@ from pierpy.io_tools import create_folders
 # from io_tools import create_folders
 import os
 import logging
+import numpy as np
+import pandas as pd
 
-def get_logger(log_name, log_filepath, log_level='debug'):
+def get_logger(log_name, log_path, log_columns, log_level='debug', log_sep='|'):
     '''
     Create a logger using file_handler.
     '''
 
-    create_folders(log_filepath)
+    create_folders(log_path)
     logger = logging.getLogger(log_name)
 
     if log_level=='debug':
@@ -20,21 +22,30 @@ def get_logger(log_name, log_filepath, log_level='debug'):
 
     formatter=logging.Formatter('%(asctime)s|%(levelname)s|%(message)s')
 
-    file_handler=logging.FileHandler(log_filepath)
+    file_handler=logging.FileHandler(log_path)
     file_handler.setFormatter(formatter)
     file_handler.setLevel(logging.INFO)
     logger.addHandler(file_handler)
 
+    try:
+        pd.read_csv(log_path, sep=log_sep)
+    except Exception as e:
+        print(f'{log_path} is not found. Start logging into a new file.')
+        print(e)
+        logger.info(log_formatter(dict(zip(log_columns, log_columns)), log_columns))
+        
     return logger
 
 
-def log_formatter(list_log_messages):
+def log_formatter(dict_log_messages, log_columns, sep='|'):
     '''
-    Formatter of messages to add into log as cells. Cells are separated by '|' (pipe).
+    Formatter of a dict messages to add into log as columns. Columns are separated by '|' (pipe).
     '''
-    return '|'.join(list_log_messages)
+    list_log_messages = [dict_log_messages.get(col, '') for col in log_columns]
+    assert(len(list_log_messages) == len(log_columns))
 
+    return sep.join(np.array(list_log_messages).astype(str))
 
-def shutdown():
+def log_shutdown():
     logging.shutdown()
     
