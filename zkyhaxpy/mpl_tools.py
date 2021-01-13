fe8f9f00e57e2031c03086dc63af3c6141b564b2
Surasak C <surasak.cho@gmail.com>
2021-01-13 17:39:48 +0700

    Add mpl_tools.auto_adjust

--- zkyhaxpy/mpl_tools.py
+++ zkyhaxpy/mpl_tools.py
@@ -5,3 +5,7 @@ def set_thai_font(font_family='tahoma'):
 
 def set_bigger_font(font_size=24):
     plt.rcParams.update({'font.size': font_size})
+    
+def auto_adjust():
+    set_thai_font()
+    set_bigger_font()
