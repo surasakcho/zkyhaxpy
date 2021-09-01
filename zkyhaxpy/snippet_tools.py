298831ea370de880b7bdb751b5b7329e1bf7b833
Surasak C <surasak.cho@gmail.com>
2021-09-01 13:25:23 +0700

    Add new module snippet_tools

---
+++ zkyhaxpy/snippet_tools.py
@@ -0,0 +1,23 @@
+crop_snippet = [    
+    '!pip install rasterio utm geopandas',
+    ''
+    'from zkyhaxpy import io_tools, gis_tools, colab_tools',
+    'import pandas as pd',
+    'import numpy as np',
+    'import os',
+    'import matplotlib.pyplot as plt',  
+    ''
+    'ln -s /home/james james'
+]
+
+def print_snippet(module):
+    '''
+    Show snippet for specified module
+
+    Available modules
+    - crop    
+    
+    '''
+    if module=='crop':
+        for txt in crop_snippet:
+            print(txt)
