4c0b01209c7b8596a22a0ab7a58b165cdbd484b1
surasak.cho <surasak.cho@gmail.com>
2022-02-14 14:27:38 +0700

    Minor updates

---
+++ zkyhaxpy/__init__.py
@@ -0,0 +1,2 @@
+print('from zkyhaxpy import snippet_tools')
+print("snippet_tools.print_snippet('module')")
--- zkyhaxpy/snippet_tools.py
+++ zkyhaxpy/snippet_tools.py
@@ -1,14 +1,15 @@
 crop_snippet = [    
     '!pip install rasterio utm geopandas',
-    ''
-    'from zkyhaxpy import io_tools, pd_tools, np_tools, console_tools, timer_tools, json_tools, dict_tools, gis_tools, colab_tools',
+    '',
+    'from zkyhaxpy import io_tools, pd_tools, np_tools, console_tools, timer_tools, json_tools, dict_tools, gis_tools',
     'import pandas as pd',
     'import numpy as np',
     'from tqdm.notebook import tqdm',
     'import os',
     'import matplotlib.pyplot as plt',  
     'import seaborn as sns',
-    '',
+    '',    
+    'from zkyhaxpy import colab_tools',
     'colab_tools.mount_drive()',
     '!ln -s /content/drive/MyDrive/!Surasak-PIER/Crop-Insurance/NDA-Data/vew_plant_info_official_polygon_disaster_all_rice_by_year /plant_info',    
     '!ln -s /content/drive/MyDrive/!PIER /!PIER',
