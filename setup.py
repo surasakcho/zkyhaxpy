8f4c7801d988e49990c028398c7547f0fd2ce0d7
Surasak C <surasak.cho@gmail.com>
2023-07-18 13:31:43 +0700

    Minor update snippet

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zkyhaxpy", # Replace with your own username
-    version="0.3.0.2.0",
+    version="0.3.0.3.0",
     author="Surasak Choedpasuporn",
     author_email="surasak.cho@gmail.com",
     description="A swiss-knife Data Science package for python",
--- zkyhaxpy/snippet_tools.py
+++ zkyhaxpy/snippet_tools.py
@@ -1,8 +1,10 @@
-crop_snippet = [    
-    '## if new envi, install these libs ##',
-    '#!pip install rasterio utm geopandas piercrop',    
+crop_snippet = [        
+    '## for all ##',    
+    "path_config = PATH",
+    'from ConfigParser import SafeConfigParser',
+    'parser = SafeConfigParser()',
+    'parser.read(path_config)',    
     '',
-    '## for all ##',
     'from zkyhaxpy import io_tools, pd_tools, np_tools, console_tools, timer_tools, json_tools, dict_tools',        
     'import pandas as pd',
     'import numpy as np',
@@ -12,20 +14,19 @@ crop_snippet = [
     'import re',
     'import matplotlib.pyplot as plt',  
     'import seaborn as sns',
-    
     '',
     '## for gis tasks##',
     'from zkyhaxpy import gis_tools',
     'import geopandas as gpd',
-    'import rasterio',
-    
+    'import rasterio',    
     '',
     '## for colab ##',
     'from zkyhaxpy import colab_tools',
     'colab_tools.mount_drive()',
-    '!ln -s /content/drive/MyDrive/!Surasak-PIER/Crop-Insurance/NDA-Data/vew_plant_info_official_polygon_disaster_all_rice_by_year /plant_info',    
-    '!ln -s /content/drive/MyDrive/!PIER /!PIER',
-    '!ln -s /content/drive/MyDrive/!Surasak-PIER/Crop-Insurance /Crop-Insurance',
+    
+    '## if new envi, install these libs ##',
+    '#!pip install rasterio utm geopandas piercrop',    
+    '',
 ]
 
 def print_snippet(module):
