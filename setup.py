67e068220c77e0af21d77d5b3756fa6c72cd7a59
Surasak C <surasak.cho@gmail.com>
2023-09-17 16:56:08 +0700

    Add log_tools.setup_logger as a new way of setup logger

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zkyhaxpy", # Replace with your own username
-    version="0.3.0.3.2",
+    version="0.3.0.3.3",
     author="Surasak Choedpasuporn",
     author_email="surasak.cho@gmail.com",
     description="A swiss-knife Data Science package for python",
